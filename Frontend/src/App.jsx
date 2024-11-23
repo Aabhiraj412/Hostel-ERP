40cea817d142e1dd762faafe1e80ebca89005fc2
Aabhiraj412 <aabhiraj412@gmail.com>
2024-11-23 12:14:03 +0530

    Routes Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
 
 export default function App() {
   return (
     // Dont Touch this File
     <div>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
