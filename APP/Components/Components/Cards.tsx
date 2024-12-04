480a543f2cefca6461f003bc44d300cfe76f4354
Aabhiraj412 <aabhiraj412@gmail.com>
2024-12-04 16:45:22 +0530

    Application : 80%

--- APP/Components/Components/Cards.tsx
+++ APP/Components/Components/Cards.tsx
@@ -2,58 +2,59 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 interface CardProps {
-  title: string;
-  onPress: () => void;
-  IconComponent: React.FC<{ size: number; color: string }>; // Accept icon as a component prop
+	title: string;
+	onPress: () => void;
+	IconComponent: React.FC<{ size: number; color: string }>; // Accept icon as a component prop
 }
 
 const Card: React.FC<CardProps> = ({ title, onPress, IconComponent }) => {
-  return (
-    <TouchableOpacity style={styles.cardContainer} onPress={onPress}>
-      <View style={styles.iconContainer}>
-        {/* Render the passed icon */}
-        <IconComponent size={40} color="#2cb5a0" />
-      </View>
-      <Text style={styles.cardText}>{title}</Text>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity style={styles.cardContainer} onPress={onPress}>
+			<View style={styles.iconContainer}>
+				{/* Render the passed icon */}
+				<IconComponent size={40} color="#2cb5a0" />
+			</View>
+			<Text style={styles.cardText}>{title}</Text>
+		</TouchableOpacity>
+	);
 };
 
 const styles = StyleSheet.create({
-  cardContainer: {
-    backgroundColor: "#2cb5a0",
-    width: 200,
-    padding: 20,
-    borderWidth: 4,
-    borderColor: "#7cdacc",
-    borderRadius: 10,
-    shadowColor: "#cfd4de",
-    shadowOffset: { width: 0, height: 6 },
-    shadowOpacity: 0.3,
-    shadowRadius: 8,
-    elevation: 5,
-    alignItems: "center",
-    justifyContent: "center",
-    marginVertical: 15,
-  },
-  iconContainer: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    borderWidth: 4,
-    borderColor: "#7cdacc",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 10,
-    backgroundColor: "#fff", // White background for the icon
-  },
-  cardText: {
-    color: "#fff",
-    fontWeight: "600",
-    fontSize: 18,
-    textAlign: "center",
-    marginTop: 10,
-  },
+	cardContainer: {
+		backgroundColor: "#2cb5a0",
+		width: 300,
+		padding: 40,
+		borderWidth: 4,
+		borderColor: "#7cdacc",
+		borderRadius: 10,
+		shadowColor: "#cfd4de",
+		shadowOffset: { width: 0, height: 6 },
+		shadowOpacity: 0.3,
+		shadowRadius: 8,
+		elevation: 5,
+		alignItems: "center",
+		justifyContent: "center",
+		marginVertical: 15,
+		marginBottom: 30,
+	},
+	iconContainer: {
+		width: 80,
+		height: 80,
+		borderRadius: 40,
+		borderWidth: 4,
+		borderColor: "#7cdacc",
+		alignItems: "center",
+		justifyContent: "center",
+		marginBottom: 10,
+		backgroundColor: "#fff", // White background for the icon
+	},
+	cardText: {
+		color: "#fff",
+		fontWeight: "600",
+		fontSize: 18,
+		textAlign: "center",
+		marginTop: 10,
+	},
 });
 
 export default Card;
--- APP/Components/Landing.tsx
+++ APP/Components/Landing.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
-import { LinearGradient } from "expo-linear-gradient"; // Importing the LinearGradient component
-import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons"; // Example icon libraries
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 import useStore from "../Store/Store";
 import Card from "./Components/Cards";
 
@@ -9,43 +9,55 @@ const Landing: React.FC<{ navigation: any }> = ({ navigation }) => {
   const { user, cookie } = useStore();
 
   const navigateToWarden = () => {
-    if (cookie && user === "Warden") 
-		navigation.reset({
-			index: 0, // Set the index to 0 to make the new screen the first screen in the stack
-			routes: [{ name: 'Warden Dashboard' }], // Provide the name of the screen you want to navigate to
-		  });
-    else navigation.navigate("Warden Login");
+    if (cookie && user === "Warden") {
+      navigation.reset({
+        index: 0,
+        routes: [{ name: "Warden Dashboard" }],
+      });
+    } else {
+      navigation.navigate("Warden Login");
+    }
   };
 
   const navigateToHostler = () => {
-    if (cookie && user === "Hostler") 
-		navigation.reset({
-			index: 0, // Set the index to 0 to make the new screen the first screen in the stack
-			routes: [{ name: 'Home Dashboard' }], // Provide the name of the screen you want to navigate to
-		  });
-    else navigation.navigate("Hostler Login");
+    if (cookie && user === "Hostler") {
+      navigation.reset({
+        index: 0,
+        routes: [{ name: "Home Dashboard" }],
+      });
+    } else {
+      navigation.navigate("Hostler Login");
+    }
   };
 
   return (
     <LinearGradient
-      colors={["#ffffff", "#ffffff"]} // Gradient colors
+      colors={["#ffffff", "#f0f8ff"]}
       style={styles.container}
     >
-      <Text style={styles.welcomeText}>Welcome</Text> {/* Welcome Title */}
-      <Card
-        title="Warden"
-        onPress={navigateToWarden}
-        IconComponent={({ size, color }) => (
-          <FontAwesome name="user-secret" size={size} color={color} />
-        )}
-      />
-      <Card
-        title="Hostler"
-        onPress={navigateToHostler}
-        IconComponent={({ size, color }) => (
-          <MaterialCommunityIcons name="account-group" size={size} color={color} />
-        )}
-      />
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <Text style={styles.welcomeText}>
+          Welcome !!
+        </Text>
+        <Card
+          title="Warden"
+          onPress={navigateToWarden}
+          IconComponent={({ size, color }) => (
+            <FontAwesome name="user-secret" size={size} color={color} />
+          )}
+        />
+        <Card
+          title="Hostler"
+          onPress={navigateToHostler}
+          IconComponent={({ size, color }) => (
+            <MaterialCommunityIcons
+              name="account-group"
+              size={size}
+              color={color}
+            />
+          )}
+        />
+      </ScrollView>
     </LinearGradient>
   );
 };
@@ -55,6 +67,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  scrollContent: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 50,
   },
   welcomeText: {
     fontSize: 36,
