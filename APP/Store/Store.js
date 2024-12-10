21af4cc23a5c26b1bd6cded7ca4c9c078b7ffe85
Aabhiraj412 <aabhiraj412@gmail.com>
2024-12-10 14:24:22 +0530

    Minor Fixes

--- APP/Store/Store.js
+++ APP/Store/Store.js
@@ -1,16 +1,30 @@
 import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persist } from "zustand/middleware";
 
-const useStore = create((set) => ({
-    cookie: '',
-    setCookie: (c) => set({ cookie: c }),
-    user: '',
-    setUser: (name) => set({ user: name }),
-    data: null,
-    setData: (d) => set({ data: d }),
-    localhost: null,
-    setLocalhost: (h) => set({ localhost: h }),
-    testlocalhost: null,
-    setTestLocalhost: (t) => set({ testlocalhost: t }),
-}));
+const useStore = create(
+  persist(
+    (set) => ({
+      cookie: "",
+      setCookie: (c) => set({ cookie: c }),
+
+      user: "",
+      setUser: (name) => set({ user: name }),
+
+      data: null,
+      setData: (d) => set({ data: d }),
+
+      localhost: null,
+      setLocalhost: (h) => set({ localhost: h }),
+
+      testlocalhost: null,
+      setTestLocalhost: (t) => set({ testlocalhost: t }),
+    }),
+    {
+      name: "zustand-storage", // Key to store data in AsyncStorage
+      getStorage: () => AsyncStorage, // Use AsyncStorage for persistence
+    }
+  )
+);
 
 export default useStore;
